c7cbff621810e507c8cef141db3f829161052377
592363789 <592363789@qq.com>
2018-03-23 15:49:08 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/parser/RateCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RateCommandParserTest.java
@@ -13,9 +13,9 @@ import seedu.address.logic.commands.RateCommand;
 import seedu.address.model.book.Rating;
 
 public class RateCommandParserTest {
-    private RateCommandParser parser = new RateCommandParser();
     private static final int EMPTY_RATING = -1;
     private static final int NON_EMPTY_RATING = 5;
+    private RateCommandParser parser = new RateCommandParser();
 
     @Test
     public void parse_indexSpecified_success() throws Exception {
