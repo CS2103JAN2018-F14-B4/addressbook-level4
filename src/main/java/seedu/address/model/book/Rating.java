01e0f8c4080a8f8eaa4dfd83683e5bc84163a943
takuyakanbr <takuyakanbr@gmail.com>
2018-03-25 22:28:09 +0800

    Update UI to display status, priority, and rating

--- src/main/java/seedu/address/model/book/Rating.java
+++ src/main/java/seedu/address/model/book/Rating.java
@@ -2,6 +2,8 @@ package seedu.address.model.book;
 
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.util.Arrays;
+
 /**
  * Represents a book's rating.
  * Guarantees: immutable.
@@ -9,9 +11,15 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Rating {
 
     public static final String MESSAGE_RATING_CONSTRAINTS = "Rating must be between -1 and 5 (both inclusive).";
-    public static final int DEFAULT_RATING = -1;
-    public static final int MIN_RATING = -1;
+    public static final int UNRATED_RATING = -1;
+    public static final int MIN_RATING = 0;
     public static final int MAX_RATING = 5;
+    public static final int DEFAULT_RATING = UNRATED_RATING;
+
+    private static final char STAR = '\u2B50';
+    private static final String DISPLAY_TEXT_UNRATED = "\u2B50 Unrated";
+    private static final String STYLE_CLASS_UNRATED = "rating-unrated";
+    private static final String STYLE_CLASS_RATED = "rating-rated";
 
     public final int rating;
 
@@ -36,7 +44,25 @@ public class Rating {
      * Returns true if the given integer is a valid rating.
      */
     public static boolean isValidRating(int rating) {
-        return rating >= MIN_RATING && rating <= MAX_RATING;
+        return rating == UNRATED_RATING || (rating >= MIN_RATING && rating <= MAX_RATING);
+    }
+
+    public String getDisplayText() {
+        if (rating == UNRATED_RATING) {
+            return DISPLAY_TEXT_UNRATED;
+        }
+
+        // returns a string with number of stars equal to rating
+        char[] repeat = new char[rating];
+        Arrays.fill(repeat, STAR);
+        return new String(repeat);
+    }
+
+    public String getStyleClass() {
+        if (rating == UNRATED_RATING) {
+            return STYLE_CLASS_UNRATED;
+        }
+        return STYLE_CLASS_RATED;
     }
 
     @Override
