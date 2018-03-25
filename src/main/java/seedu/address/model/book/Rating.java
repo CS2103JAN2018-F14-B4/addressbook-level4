118b7c9b42cb2f24b2399fca862d269cb695e098
takuyakanbr <takuyakanbr@gmail.com>
2018-03-25 21:30:02 +0800

    Add Priority and Status fields to Book

--- src/main/java/seedu/address/model/book/Rating.java
+++ src/main/java/seedu/address/model/book/Rating.java
@@ -1,6 +1,6 @@
 package seedu.address.model.book;
 
-import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a book's rating.
@@ -8,33 +8,52 @@ import static java.util.Objects.requireNonNull;
  */
 public class Rating {
 
-    public final Integer rating;
+    public static final String MESSAGE_RATING_CONSTRAINTS = "Rating must be between -1 and 5 (both inclusive).";
+    public static final int DEFAULT_RATING = -1;
+    public static final int MIN_RATING = -1;
+    public static final int MAX_RATING = 5;
+
+    public final int rating;
+
+    /**
+     * Contructs a {@code Rating} with the default rating.
+     */
+    public Rating() {
+        this.rating = DEFAULT_RATING;
+    }
 
     /**
      * Constructs a {@code Rating}.
      *
      * @param rating A book rating.
      */
-    public Rating(Integer rating) {
-        requireNonNull(rating);
+    public Rating(int rating) {
+        checkArgument(isValidRating(rating), MESSAGE_RATING_CONSTRAINTS);
         this.rating = rating;
+    }
+
+    /**
+     * Returns true if the given integer is a valid rating.
+     */
+    public static boolean isValidRating(int rating) {
+        return rating >= MIN_RATING && rating <= MAX_RATING;
     }
 
     @Override
     public String toString() {
-        return rating.toString();
+        return Integer.toString(rating);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Rating // instanceof handles nulls
-                && this.rating.equals(((Rating) other).rating)); // state check
+                && this.rating == ((Rating) other).rating); // state check
     }
 
     @Override
     public int hashCode() {
-        return rating.hashCode();
+        return Integer.hashCode(rating);
     }
 
 }
