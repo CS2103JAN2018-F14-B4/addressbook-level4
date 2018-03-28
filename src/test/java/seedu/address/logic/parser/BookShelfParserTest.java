1576dc0051d24695416d456a57940afc20094e92
592363789 <592363789@qq.com>
2018-03-29 00:33:44 +0800

    fix checkstyle errors

--- src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
+++ src/test/java/seedu/address/logic/parser/BookShelfParserTest.java
@@ -7,7 +7,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_RATING;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_BOOK;
 
 import org.junit.Rule;
