9da2c756e9f67903d3b38f3e5bf6256dfc34d9de
592363789 <592363789@qq.com>
2018-04-12 20:35:41 +0800

    update developguide

---
+++ src/main/java/seedu/address/logic/KeyControl.java
@@ -0,0 +1,36 @@
+package seedu.address.logic;
+
+public class KeyControl {
+
+    private static String key;
+    private Boolean isEncrypt = false;
+
+    public KeyControl(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public boolean getEncrypt() {
+        return isEncrypt;
+    }
+
+    public void setKey(String word) {
+        key = word;
+    }
+
+    public void encrypt() {
+        isEncrypt = true;
+    }
+
+    public void decrypt() {
+        isEncrypt = false;
+    }
+
+    public void setIsEncrypt(boolean isEncrypt) {
+        this.isEncrypt = isEncrypt;
+    }
+
+}
