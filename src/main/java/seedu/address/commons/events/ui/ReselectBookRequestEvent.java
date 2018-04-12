650d7ce0d95c6f0539033491e2798b92c48619c8
qiu-siqi <cuteyingmei@gmail.com>
2018-04-13 05:36:22 +0800

    Change wording

--- src/main/java/seedu/address/commons/events/ui/ReselectBookRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/ReselectBookRequestEvent.java
@@ -3,7 +3,7 @@ package seedu.address.commons.events.ui;
 import seedu.address.commons.events.BaseEvent;
 
 /**
- * Indicates a request to reselect a book, if it was previously deselected using {@link DeselectBookRequestEvent}.
+ * Indicates a request to reselect the book that was previously deselected using {@link DeselectBookRequestEvent}.
  */
 public class ReselectBookRequestEvent extends BaseEvent {
 
